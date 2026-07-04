ec084815f462ece2e87179910938f17071528e96
habiba <habibaibrahum@gmail.com>
2026-07-04 21:28:37 +0300

    Update login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./login";
+import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
 import Profile from "./Profile";
